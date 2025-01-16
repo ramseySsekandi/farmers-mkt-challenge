f69f814b5a74e5dbbf4a3f0bc80f6fb21659610c
ramseySsekandi <moshehravens@gmail.com>
2025-01-16 17:54:13 +0300

    just commiting

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 import { Bell, MessageSquareMore, ShoppingCart } from 'lucide-react'
+import Link from 'next/link'
+import Product from '@/components/Product'
 
 const Homepage = () => {
   return (
-    <div className='h-svh w-screen px-4 py-2'>
+    <div className='h-svh w-screen px-4 py-2 space-y-2'>
       {/* HEADER */}
       <div className="h-[100px] w-full flex items-center justify-between">
         {/* USER */}
@@ -34,6 +36,19 @@ const Homepage = () => {
       <div className=" flex gap-4">
       <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." required />
       <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+      </div>
+      {/* HEADING */}
+      <div className="flex justify-between">
+        <p className='text-xl font-bold'>Popular</p>
+        <Link href='/products' className='text-blue-500'>See All</Link>
+      </div>
+      {/* PRODUCTS */}
+      <div className="grid gap-1">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
       </div>
     </div>
   )
