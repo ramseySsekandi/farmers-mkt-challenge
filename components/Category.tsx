3598e2badbebcc722bf00b6e89900728c4e116dc
ramseySsekandi <moshehravens@gmail.com>
2025-01-17 15:07:58 +0300

    added some space on the category comp

--- components/Category.tsx
+++ components/Category.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Category = ({category}:{category:ICategory}) => {
   return (
     <div className='flex flex-col justify-center items-center'>
-        <div className="w-[110px] h-[100px] relative ">
+        <div className="w-[100px] h-[100px] relative ">
         <Image src={category.image} alt={category.slug} fill className='rounded-xl object-cover'/>
         </div>
         <h3 className='text-center text-sm font-medium'>{category.title}</h3>
