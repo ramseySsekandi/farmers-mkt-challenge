50e5a9fdc101ae67d8b60388433e1b20b06254f8
ramseySsekandi <moshehravens@gmail.com>
2025-01-17 12:50:24 +0300

    add cart padding vertical

--- components/Product.tsx
+++ components/Product.tsx
@@ -46,7 +46,7 @@ const Product = ({product}: {product:IProductProp}) => {
             </div>
         </div>
         <div className="w-full flex">
-        <a href="#" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Cart</a>
+        <a href="#" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Cart</a>
         </div>
     </div>
 </div>
