8299092c3a65fcac4da2d68cd85b2f34f73d3384
ramseySsekandi <moshehravens@gmail.com>
2025-01-17 14:09:55 +0300

    carousel pics updated to svg

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -30,8 +30,18 @@ const Carousel = () => {
       modules={[Pagination, Navigation]}
     >
       <SwiperSlide className='w-full h-full'>
-        <div className="w-full h-[50%] flex justify-center items-center">
-          <Image src='/mango-table.jpg' className='object-contain w-full h-full' width={100} height={200} alt='hi'/>
+        <div className="p-8 w-full h-[70%] flex justify-center items-center">
+          <Image src='/tractor.svg' className='object-contain w-full h-full' width={100} height={200} alt='hi'/>
+        </div>
+        <h2 className="text-2xl font-bold text-center">Agri Rental</h2>
+        <p className='text-sm text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Blanditiis nobis vitae porro officia saepe unde illo ullam quis,
+          dolores nesciunt.
+        </p>
+        </SwiperSlide>
+      <SwiperSlide className='w-full h-full'>
+        <div className="p-8 w-full h-[70%] flex justify-center items-center">
+          <Image src='/farmer-mkt.svg' className='object-contain w-full h-full' width={100} height={200} alt='hi'/>
         </div>
         <h2 className="text-2xl font-bold text-center">Farmers Market</h2>
         <p className='text-sm text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -39,27 +49,17 @@ const Carousel = () => {
           dolores nesciunt.
         </p>
         </SwiperSlide>
-      <SwiperSlide className='w-full h-full'>
-        <div className="w-full h-[50%] flex justify-center items-center">
-          <Image src='/mango-table.jpg' className='object-contain w-full h-full' width={100} height={200} alt='hi'/>
-        </div>
-        <h2 className="text-2xl font-bold text-center">Farmers Market</h2>
-        <p className='text-sm text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Blanditiis nobis vitae porro officia saepe unde illo ullam quis,
-          dolores nesciunt.
-        </p>
-        </SwiperSlide>
-      <SwiperSlide className='w-full h-full'>
+      <SwiperSlide className='p-8 w-full h-full'>
         <div className="w-full h-[70%] flex justify-center items-center bg-lime-400">
-          <Image src='/mango-table.jpg' className='object-cover w-full h-full' width={100} height={200} alt='hi'/>
+          <Image src='/ai-disease.svg' className='object-cover w-full h-full' width={100} height={200} alt='hi'/>
         </div>
-        <h2 className="text-2xl font-bold text-center">Farmers Market</h2>
+        <h2 className="text-2xl font-bold text-center">AI Disease Detection</h2>
         <p className='text-sm text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Blanditiis nobis vitae porro officia saepe unde illo ullam quis,
           dolores nesciunt.
         </p>
         <div className="relative">
-        <Link href='/homepage'><button className="absolute bottom-[-100px] right-1 border-blue-500 border px-10 py-2 rounded-3xl text-blue-500 flex gap-1">Skip <SkipForward /></button></Link>
+        <Link href='/homepage'><button className="absolute bottom-[-100px] right-[-24] border-blue-500 border px-10 py-2 rounded-3xl text-blue-500 flex gap-1">Skip <SkipForward /></button></Link>
         
         </div>
 
